71d47ebc223186f7a8de24c79b21441df89867a0
Kirty Meena <kirtymeena@gmail.com>
2022-03-13 14:28:14 +0530

    commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const content = document.querySelector(".content")
+const dice = document.querySelector(".dice")
+const id = document.querySelector(".num")
+let slip_id=117
+
+const getAdvice = (slip_id)=>{
+    fetch(`https://api.adviceslip.com/advice/${slip_id}`).then(
+        function(response){
+            if(response.status!==200){
+                console.log("something went wrong",response.status)
+                return;
+            }
+
+            response.json().then(function(data){
+                content.innerHTML = '<q>'+data.slip.advice+'</q>'
+                id.innerHTML = slip_id
+                console.log(data)
+            });
+        }
+    ).catch(function(err){
+        console.log("Fetch error",err)
+    })
+}
+
+dice.addEventListener('click',()=>{
+        slip_id+=1
+        getAdvice(slip_id)
+})
+
+getAdvice(slip_id)
+
+
+
+
